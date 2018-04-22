e32c35c79a063db82ea813a5b77def06ab997937
kelvin <lkflkf99@gmail.com>
2018-04-22 16:29:49 +0800

    add KNN lib

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 import csvjson from "csvjson";
 import fs from "fs";
 import path from "path";
-import brain from 'brain.js';
+import brain from "brain.js";
+import nn from "nearest-neighbor";
 
 const net = new brain.NeuralNetwork();
 
@@ -26,11 +27,48 @@ const modal = dataArr.map(element => {
     output: {
       price: element.price
     }
-  }
+  };
   return modalData;
 });
 
-console.log(modal);
-net.train(modal);
-var output = net.run({ accommodates: 1, bedrooms: 1, minstay: 1 });
-console.log(output);
+// console.log(modal);
+
+// net.train(modal);
+// var output = net.run({ accommodates: 1, bedrooms: 1, minstay: 1 });
+// console.log(output);
+
+//k nearest-neighbor
+const kNN_modal = dataArr.map(element => ({
+  room_type: element.room_type,
+  neighborhood: element.neighborhood,
+  accommodates: parseInt(element.accommodates),
+  bedrooms: parseInt(element.bedrooms),
+  minstay: parseInt(element.minstay),
+  price: parseInt(element.price)
+}));
+
+var query = {
+  room_type: "Shared room",
+  neighborhood: null,
+  accommodates: 1,
+  bedrooms: 1,
+  minstay: 1,
+  price: null
+};
+
+var fields = [
+  { name: "room_type", measure: nn.comparisonMethods.word },
+  { name: "neighborhood", measure: nn.comparisonMethods.word },
+  { name: "accommodates", measure: nn.comparisonMethods.number, max: 100 },
+  { name: "bedrooms", measure: nn.comparisonMethods.number, max: 100 },
+  { name: "minstay", measure: nn.comparisonMethods.number, max: 100 },
+  { name: "price", measure: nn.comparisonMethods.number, max: 100 }
+];
+
+nn.findMostSimilar(query, kNN_modal, fields, (nearestNeighbor, probability) => {
+  console.log(query);
+  console.log(nearestNeighbor);
+  console.log(probability);
+});
+
+// console.log(kNN_modal);
