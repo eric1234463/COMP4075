1dac7a5cda45b4fdff3f6d823ffe9deb9ba142d7
kelvin <lkflkf99@gmail.com>
2018-04-22 19:06:53 +0800

    Auto stash before merge of "master" and "origin/master"

--- app.js
+++ app.js
@@ -2,11 +2,22 @@ import csvjson from "csvjson";
 import fs from "fs";
 import path from "path";
 import DecisionTree from "decision-tree";
-import nn from "nearest-neighbor";
 import express from "express";
+import prompt from "prompt";
+import KNN from "ml-knn";
 
+//render app
 const app = express();
+app.set('view engine', 'ejs');  
+app.use(express.static('public'));
+app.get("/", (req, res) => {
+  const data = getData();
+  const knn = convertKnnModel(data);
+  res.render('index', { data: data })
+});
+app.listen(3000, () => console.log("Example app listening on port 3000!"));
 
+//DT
 const getData = () => {
   const data = fs.readFileSync(path.join(__dirname, "airbnb.csv"), {
     encoding: "utf8"
@@ -54,58 +65,106 @@ const convertKnnModel = dataArr => {
     };
     return modalData;
   });
+}
 
-  // console.log(modal);
+//k nearest-neighbor
 
-  // net.train(modal);
-  // var output = net.run({ accommodates: 1, bedrooms: 1, minstay: 1 });
-  // console.log(output);
+let knn;
+let seperationSize; // To seperate training and test data
+let X = [], y = [];
+let trainingSetX = [], trainingSetY = [], testSetX = [], testSetY = [];
 
-  //k nearest-neighbor
-  const kNN_modal = dataArr.map(element => ({
-    room_type: element.room_type,
-    neighborhood: element.neighborhood,
-    accommodates: parseInt(element.accommodates),
-    bedrooms: parseInt(element.bedrooms),
-    minstay: parseInt(element.minstay),
-    price: parseInt(element.price)
-  }));
+let kNN_modal = getData().map(element => ({
+  room_type: element.room_type,
+  accommodates: parseInt(element.accommodates),
+  bedrooms: parseInt(element.bedrooms),
+  minstay: parseInt(element.minstay),
+  price: parseInt(element.price)
+}));
 
-  var query = {
-    room_type: "Shared room",
-    neighborhood: null,
-    accommodates: 1,
-    bedrooms: 2,
-    minstay: 1,
-    price: null
-  };
+  seperationSize = 0.7 * kNN_modal.length;
+  kNN_modal = shuffleArray(kNN_modal);
+  dressData();
 
-  var fields = [
-    { name: "room_type", measure: nn.comparisonMethods.word },
-    { name: "neighborhood", measure: nn.comparisonMethods.word },
-    { name: "accommodates", measure: nn.comparisonMethods.number, max: 100 },
-    { name: "bedrooms", measure: nn.comparisonMethods.number, max: 100 },
-    { name: "minstay", measure: nn.comparisonMethods.number, max: 100 },
-    { name: "price", measure: nn.comparisonMethods.number, max: 100 }
-  ];
+function dressData() {
 
-  nn.findMostSimilar(
-    query,
-    kNN_modal,
-    fields,
-    (nearestNeighbor, probability) => {
-      console.log(query);
-      console.log(nearestNeighbor);
-      console.log(probability);
+    let types = new Set();
+
+    kNN_modal.forEach((row) => {
+        types.add(row.room_type);
+    });
+
+    let typesArray = [...types];
+    
+    // typesArray:
+    // entire home = 0
+    // private room = 1
+    // shared room = 2
+
+    kNN_modal.forEach((row) => {
+        let rowArray, typeNumber;
+
+        rowArray = Object.keys(row).map(key => parseFloat(row[key])).slice(1, 5);
+
+        typeNumber = typesArray.indexOf(row.room_type); // Convert type(String) to type(Number)
+
+        X.push(rowArray);
+        y.push(typeNumber);
+    });
+
+    trainingSetX = X.slice(0, seperationSize);
+    trainingSetY = y.slice(0, seperationSize);
+
+    testSetX = X.slice(seperationSize);
+    testSetY = y.slice(seperationSize);
+
+    train();
+}
+
+function train() {
+    knn = new KNN(trainingSetX, trainingSetY, {k: 7});
+    test();
+}
+
+function test() {
+    const result = knn.predict(testSetX);
+    const testSetLength = testSetX.length;
+    const predictionError = error(result, testSetY);
+    console.log(`Test Set Size = ${testSetLength} and number of Misclassifications = ${predictionError}`);
+    predict();
+}
+
+function error(predicted, expected) {
+    let misclassifications = 0;
+    for (var index = 0; index < predicted.length; index++) {
+        if (predicted[index] !== expected[index]) {
+            misclassifications++;
+        }
     }
-  );
-};
-app.set('view engine', 'ejs');  
-app.use(express.static('public'));
-app.get("/", (req, res) => {
-  const data = getData();
-  const knn = convertKnnModel(data);
-  res.render('index', { data: data })
-});
+    return misclassifications;
+}
 
-app.listen(3000, () => console.log("Example app listening on port 3000!"));
+function predict() {
+    let temp = [];
+    prompt.start();
+    
+    prompt.get(['accommodates', 'bedrooms', 'minstay', 'price'], function (err, result) {
+        if (!err) {
+            for (var key in result) {
+                temp.push(parseFloat(result[key]));
+            }
+            console.log(`With ${temp} -- room_type =  ${knn.predict(temp)}`);
+        }
+    });
+}
+
+function shuffleArray(array) {
+  //for random array order
+    for (var i = array.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+    return array;
+}
